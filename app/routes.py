d697077475d594b348b1376e56d775bead7a1125
Alex Kagai <alexkagai@outlook.com>
2018-02-26 18:20:08 +0300

    set up user login with actual Database

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -25,11 +27,17 @@ def user(name):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     login_form = LoginForm()
 
     if login_form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            login_form.username.data, login_form.remember_me.data))
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user is None or not user.check_password(login_form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=login_form.remember_me.data)
         return redirect(url_for('index'))
 
     return render_template('login.html', title='Sign In', form=login_form)
