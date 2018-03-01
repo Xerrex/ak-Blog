dfef4a3dd7d9a0444d333d27e1ae138a0aba514f
Alex Kagai <alexkagai@outlook.com>
2018-03-01 12:18:09 +0300

    add User registration:Form, Route, Template

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,8 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 from app import app
-from app.forms import LoginForm
+from app import db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -55,3 +56,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        user = User(username=reg_form.username.data, email=reg_form.email.data)
+        user.set_password(reg_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=reg_form)
