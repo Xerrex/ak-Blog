0d65428b7ca66994e7687c58465ff2edeebafc93
Alex Kagai <alexkagai@outlook.com>
2018-03-01 14:26:07 +0300

    Add User profile page

--- app/routes.py
+++ app/routes.py
@@ -25,9 +25,15 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
