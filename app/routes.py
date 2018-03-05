3d9db57b7848929fc6aede782acb21440563c533
Alex Kagai <alexkagai@outlook.com>
2018-03-05 12:09:57 +0300

    fix duplicate username bug

--- app/routes.py
+++ app/routes.py
@@ -91,7 +91,7 @@ def register():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    edit_profile_form = EditProfileForm()
+    edit_profile_form = EditProfileForm(current_user.username)
     if edit_profile_form.validate_on_submit():
         current_user.username = edit_profile_form.username.data
         current_user.about_me = edit_profile_form.about_me.data
