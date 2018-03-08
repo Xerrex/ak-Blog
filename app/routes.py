5d66f2303a236ee61625d2f27a79b631fe6f0f24
Alex Kagai <alexkagai@outlook.com>
2018-03-08 10:49:21 +0300

    Add blog form to template and routes to handle blogs

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 from app import app
 from app import db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 
-from app.models import User
+from app.models import User, Post
 
 
 @app.before_request
@@ -17,10 +17,18 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    postform = PostForm()
+
+    if postform.validate_on_submit():
+        post = Post(body=postform.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'Nick'},
@@ -31,7 +39,8 @@ def index():
             'body': 'Kenya my Country'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user,
+                           posts=posts, form=postform)
 
 
 @app.route('/user/<username>')
@@ -135,3 +144,4 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
