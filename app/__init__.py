83c1bd0559afe4b9e76901f4ea4af93e7e2c71a9
Alex Kagai <alexkagai@outlook.com>
2018-03-12 21:58:44 +0300

    Initialize app with Babel

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -21,6 +22,7 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
