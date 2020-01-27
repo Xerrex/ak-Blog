0ad019f094d5f14c4769884160b6f2c2eeb3491f
Alex kagai <alexkagai@outlook.com>
2020-01-27 18:33:54 +0300

    Update flask shell context

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, cli, db
-from app.models import User, Post, Message, Notification
+from app.models import User, Post, Message, Notification, Task
 
 app_environment = os.getenv('FLASK_CONFIG') or 'production'
 app = create_app(app_environment)
@@ -9,5 +9,12 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification}
+    models = {
+        'db': db, 
+        'User': User, 
+        'Post': Post, 
+        'Message': Message,
+        'Notification': Notification,
+        'Task': Task
+    }
+    return models
